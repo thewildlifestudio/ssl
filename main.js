ce669cd0fdff794974d9a2b80214a651dc46e824
wildlife studio <sourav.seoinfotechsolution@gmail.com>
2018-09-12 18:00:45 +0530

    added ame3

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-console.log('loading.');
-// window.location.href = 'https://www.tripadvisor.com/';
+/* 
+
+<meta property="og:locale" content="en_GB" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="The Wildlife Studio by Alex Jones" />
+<meta property="og:description" content="The Wildlife Studio Shop by Alex Jones. I has built an unrivalled reputation for capturing the beauty of the planet&#039;s remote landscapes, cultures and endangered animals." />
+<meta property="og:url" content="https://thewildlife.studio/" />
+<meta property="og:site_name" content="The Wildlife Studio" />
+
+<style type="text/css">
+img.wp-smiley,
+img.emoji {
+	display: inline !important;
+	border: none !important;
+	box-shadow: none !important;
+	height: 1em !important;
+	width: 1em !important;
+	margin: 0 .07em !important;
+	vertical-align: -0.1em !important;
+	background: none !important;
+	padding: 0 !important;
+}
+</style>
+
+*/
